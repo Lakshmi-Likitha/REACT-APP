223fb034a8276172dc47cce556a89774b1db8a52
Lakshmi Likitha Malemarpuram <131754974+Lakshmi-Likitha@users.noreply.github.com>
2024-07-25 01:27:57 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,11 +11,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "reactchat-4dc68.firebaseapp.com",
-  projectId: "reactchat-4dc68",
-  storageBucket: "reactchat-4dc68.appspot.com",
-  messagingSenderId: "312714301777",
-  appId: "1:312714301777:web:84aa2c3ee010a9a59d8370"
+  authDomain: "reactchatapplication-ffd93.firebaseapp.com",
+  projectId: "reactchatapplication-ffd93",
+  storageBucket: "reactchatapplication-ffd93.appspot.com",
+  messagingSenderId: "336828778799",
+  appId: "1:336828778799:web:85d2e43bc5ac54b8e1b9da",
+  measurementId: "G-NBV2XP3L6G"
 };
 
 // Initialize Firebase
